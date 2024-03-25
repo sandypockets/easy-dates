49463342b195ecb14413a2aed173e957e13bc426
sandypockets <41454557+sandypockets@users.noreply.github.com>
2024-03-25 00:33:39 -0400

    Update year

--- src/getYear/getYear.test.js
+++ src/getYear/getYear.test.js
@@ -1,5 +1,5 @@
 const { getYear } = require("../../dist");
 
-test("expect the current year to be 2022", () => {
-  expect(getYear()).toBe(2022);
+test("expect the current year to be 2024", () => {
+  expect(getYear()).toBe(2024);
 });
